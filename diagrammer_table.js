b8da9362209290be6b069f594f8c39d5c92fb494
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-09 02:29:41 +0100

    first commit

---
+++ diagrammer_table.js
@@ -0,0 +1,9 @@
+const pool = require('./mysql_pool.js')
+
+async function diagrammer_table(){
+    const [data] = await pool.execute('SELECT * FROM diagrammer')
+
+    return data
+} 
+
+module.exports = diagrammer_table
