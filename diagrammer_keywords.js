f8da990a5662f2a7ab07df4594b799abe071c2a2
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-09 02:29:26 +0100

    first commit

---
+++ diagrammer_keywords.js
@@ -0,0 +1,25 @@
+const pool = require('./mysql_pool.js')
+
+diagrammer_keywords = async () => {
+    const keywordSet = new Set();
+
+    const [rows] = await pool.execute(`SELECT keywords from diagrammer`);
+
+    rows.forEach((row) => {
+        if (row.keywords) {
+            let keywordRow = row.keywords.match(",")
+                ? row.keywords.split(",")
+                : row.keywords;
+
+            if (toString.call(keywordRow) === "[object Array]")
+                keywordRow.forEach((keyword) => {
+                    if (keyword) keywordSet.add(keyword.trim());
+                });
+            else keywordSet.add(keywordRow.trim());
+        }
+    });
+
+    return keywordSet
+};
+
+module.exports = diagrammer_keywords
